0f6b18f34b9ea3957ffdac53d753e79c436e447e
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2023-09-16 11:37:48 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -6,7 +6,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
- */
+ * sssss */
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
