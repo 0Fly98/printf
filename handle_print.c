51d1e128a2bbe31aba632e61dd0c421a676fb741
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2023-09-16 16:06:32 +0300

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -25,7 +25,7 @@
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknow_len = 0, printed_chars = -1;
+	int i, len = 0, chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -41,9 +41,9 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-		unknow_len += write(1, "%%", 1);
+		len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
-			unknow_len += write(1, " ", 1);
+			len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
@@ -53,8 +53,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 				--(*ind);
 			return (1);
 		}
-		unknow_len += write(1, &fmt[*ind], 1);
-		return (unknow_len);
+		len += write(1, &fmt[*ind], 1);
+		return (len);
 	}
-	return (printed_chars);
+	return (chars);
 }
