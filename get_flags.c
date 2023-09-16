a9fd68f3cb76e3e37f3ea6cbbc54999f494958b6
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2023-09-16 12:19:00 +0300

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
+ * get_flags - Retrieves formatting flags from a format string.
+ * @format: The format string being parsed.
+ * @i: A pointer to the current position in the format string.
+ *
+ * This function searches for and collects formatting flags from the format
+ * string, updating the current position pointer accordingly.
+ *
+ * Return: An integer representing the collected flags.
  */
+
 int get_flags(const char *format, int *i)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8 16 */
 	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
@@ -18,11 +21,13 @@ int get_flags(const char *format, int *i)
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
 		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+		{
 			if (format[curr_i] == FLAGS_CH[j])
 			{
 				flags |= FLAGS_ARR[j];
 				break;
 			}
+		}
 
 		if (FLAGS_CH[j] == 0)
 			break;
