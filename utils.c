46290f8c3e9b94fae0594d3421c36efb3a9d532c
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2023-09-16 16:22:41 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -33,7 +33,7 @@ int is_printable(char c)
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-	char map_to[] = "0123456789ABCDEF";
+	char map[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
@@ -41,8 +41,8 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
 
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
+	buffer[i++] = map[ascii_code / 16];
+	buffer[i] = map[ascii_code % 16];
 
 	return (3);
 }
