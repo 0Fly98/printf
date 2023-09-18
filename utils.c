7d58555211c12dab9cc7a2c424a54deae4d249cf
joshua lundu <122810726+0Fly98@users.noreply.github.com>
2023-09-18 18:39:14 +0200

    utils.c

--- utils.c
+++ utils.c
@@ -1,22 +1,16 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
  * is_printable - Checks if a character is printable.
  * @c: The character to check.
  *
- * This function determines whether a character
- *			is a printable character, i.e., it falls within
- * the ASCII range of 32 to 126, inclusive.
- *
  * Return: 1 if the character is printable, 0 otherwise.
  */
-
 int is_printable(char c)
 {
-	if (c >= 32 && c < 127)
-		return (1);
-
-	return (0);
+	return (c >= 32 && c <= 126);
 }
 
 /**
@@ -25,24 +19,17 @@ int is_printable(char c)
  * @buffer: The character array where the data is stored.
  * @i: The index at which the escape sequence should be appended.
  *
- * This function appends a hexadecimal escape sequence
- *			(e.g., "\xHH") to the buffer for a given
- * ASCII code. It returns the number of characters appended, which is always 3.
- *
  * Return: The number of characters appended to the buffer.
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map[] = "0123456789ABCDEF";
-	/* The hexa format code is always 2 digits long */
-	if (ascii_code < 0)
-		ascii_code *= -1;
 
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
 
-	buffer[i++] = map[ascii_code / 16];
-	buffer[i] = map[ascii_code % 16];
+	buffer[i++] = map[(ascii_code >> 4) & 0x0F];
+	buffer[i] = map[ascii_code & 0x0F];
 
 	return (3);
 }
@@ -51,19 +38,11 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  * is_digit - Checks if a character is a digit.
  * @c: The character to check.
  *
- * This function determines whether a character
- *			is a digit by checking if it falls within
- * the ASCII range of '0' to '9'.
- *
  * Return: 1 if the character is a digit, 0 otherwise.
  */
-
 int is_digit(char c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-
-	return (0);
+	return (c >= '0' && c <= '9');
 }
 
 /**
@@ -71,14 +50,8 @@ int is_digit(char c)
  * @num: The number to be converted.
  * @size: The size modifier (e.g., S_LONG, S_SHORT).
  *
- * This function converts a number to a different
- *			data type based on the specified size.
- * It can be used to convert a long integer (S_LONG),
- *				short integer (S_SHORT), or int (default).
- *
  * Return: The converted number.
  */
-
 long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
@@ -95,16 +68,9 @@ long int convert_size_number(long int num, int size)
  * @num: The unsigned number to be converted.
  * @size: The size modifier (e.g., S_LONG, S_SHORT).
  *
- * This function converts an unsigned number to
- *				a different data type based on the specified size.
- * It can be used to convert an unsigned long integer
- *			(S_LONG), unsigned short integer (S_SHORT), or
- *				unsigned int (default).
- *
  * Return: The converted unsigned number.
  */
-
-long int convert_size_unsgnd(unsigned long int num, int size)
+unsigned long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
